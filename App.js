9599df268b9fc390f8d64b58bbe1d8379fffc92a
Krismar Faria <krismarfaria27@gmail.com>
2022-04-10 20:13:38 +0200

    Tab

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { StyleSheet, Text, View, Pressable } from "react-native";
+import Icon from "react-native-ico-material-design";
+import React from "react";
+
+
+
+var iconHeight = 26;
+var iconWidth = 26;
+
+export default class App extends React.Component {
+
+
+
+
+
+
+
+  state = {
+    screenText: "Press a button",
+  };
+
+  changeText = (text) => {
+    console.log(text + "has been pressed");
+    this.setState({
+      screenText: text,
+    });
+  };
+
+  render() {
+    return (
+     <View style={styles.container}>
+       <View>
+         <Text style={{fontSize:30, color:'white'}}>{this.state.screenText}</Text>
+         <StatusBar style="light"/>
+       </View> 
+
+<View style={styles.navContainer}>
+  <View style={styles.navBar}>
+
+
+<Pressable onPress={() => this.changeText('camera')} style={styles.IconBahave} android_ripple={{borderless:true, radius:50}}>
+ <Icon name="camera" height={iconHeight} width={iconWidth} color='#f77f00'/>
+</Pressable>
+    
+<Pressable onPress={() => this.changeText('Chat')} style={styles.IconBahave} android_ripple={{borderless:true, radius:50}}>
+ <Icon name="chat-bubble" height={iconHeight} width={iconWidth} color='#f77f00'/>
+</Pressable>
+    
+
+<Pressable onPress={() => this.changeText('user')} style={styles.IconBahave} android_ripple={{borderless:true, radius:50}}>
+ <Icon name="users-social-symbol" height={iconHeight} width={iconWidth} color='#f77f00'/>
+</Pressable>
+
+  </View>
+</View>
+
+     </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f77f00",
+    alignItems: "center",
+    justifyContent: "center",
   },
+
+  navContainer: {
+    position: 'absolute',
+    alignItems: 'center',
+    bottom: 20,
+  },
+
+
+navBar: {
+  flexDirection:'row',
+  backgroundColor:'#eee',
+  width: '90%',
+  justifyContent:'space-evenly',
+  borderRadius: 40
+},
+
+IconBahave:{
+  padding: 14
+}
+
 });
---
+++ Screens/Setting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SettingScreen = () => {
+  return (
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'} } >
+    <Text>Setting</Text>
+</View>
+  )
+}
+
+export default SettingScreen
